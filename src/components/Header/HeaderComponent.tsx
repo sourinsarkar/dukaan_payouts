36750541d0bc8526599a4b635d505cda55ffb66e
Sourin <iamsourinsarkar@gmail.com>
2024-01-09 21:30:45 +0530

    improved header structure

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -2,15 +2,24 @@ import HeaderBtnComponent from "./HeaderBtnComponent";
 
 const HeaderComponent: React.FC = () => {
     return (
-        <div>
-            <div>
-                Payments<span><img src="/images/HelpIcon.svg" alt="Help" /> <span>How it works</span></span>
+        <div className="flex items-center justify-between">
+            {/* Page Tag */}
+            <div className="w-full flex items-center">
+                Payments
+                <div className="flex items-center">
+                    <img src="/images/HelpIcon.svg" alt="Help" />
+                    How it works
+                </div> 
             </div>
-            <div>
+
+            {/* Search bar */}
+            <div className="w-full flex items-center">
                 <img src="/images/SearchIcon.svg" alt="Search" /> <span><input type="search" placeholder="Search features, tutorials, etc." /></span>
             </div>
-            <div>
-                <div>
+
+            {/* Header Buttons */}
+            <div className="w-full flex items-center justify-end">
+                <div className="flex items-center">
                     <HeaderBtnComponent image="/images/AnnouncementIcon.svg" rel="Announcement" />
                     <HeaderBtnComponent image="/images/DropDownIcon.svg" rel="Announcement" />
                 </div>
